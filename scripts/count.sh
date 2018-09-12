c07ebab489a73607060fcaeafe3db2d8f6f318e6
avantsao <avan_tsao@wistron.com>
2018-09-12 17:31:34 +0800

    feat(count.sh): Re-edit with funciton block. Add the date recording function
    feat(count.sh): modify idleAndReboot duration time as input argument () type.
    
    feat(count.sh): modify recordDate dump log name from clog.log to date.log
    
    feat(count.sh): modify recordDate() : from 'echo  >> /tmp/clog.log' to 'echo  >> /tmp/date.log'

--- scripts/count.sh
+++ scripts/count.sh
@@ -1,12 +1,33 @@
 #!/bin/sh
 
+# Recording the exectue date
+recordDate() {
+	echo $c >> /tmp/date.log
+	echo `date` >> /tmp/date.log
+	echo "=======" >> /tmp/date.log
+}
+
+# Idle 6c sec and do init 6
+idleAndReboot(){
+    sleep $1
+    init 6
+}
+
+# code main entry
+reboot_main(){
 if [ ! -e "/tmp/clog.log" ]
 then
     echo 1 > /tmp/clog.log
+	recordDate
+	idleAndReboot
 else
     c=`cat /tmp/clog.log`
     ((c++))
     echo $c > /tmp/clog.log
-    sleep 60
-    init 6
+	recordDate
+	idleAndReboot 300
 fi
+}
+
+# execution area
+reboot_main
