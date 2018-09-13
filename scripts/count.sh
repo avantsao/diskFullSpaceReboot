b1e4e307f616762d43a35cfc521b48e63b4bb59e
avantsao <avan_tsao@wistron.com>
2018-09-13 10:38:28 +0800

    feat(count.sh): 1.add beginDate() to record the test start time; 2.recode the currenttime duration

--- scripts/count.sh
+++ scripts/count.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
 
 # Recording the exectue date
+
+beginDate(){
+	[ -e "/tmp/beginTimeStamp.log" ] || { 
+		echo `date +%s` > /tmp/beginTimeStamp.log;
+		return 0;  
+	}
+}
 recordDate() {
 	echo $c >> /tmp/date.log
 	echo `date` >> /tmp/date.log
+	begin=`cat /tmp/beginTimeStamp.log`
+	lastDate=`date +%s`
+	diff=`expr $lastDate - $begin`
+	diffMin=$((diff/60))
+	diffHrs=$((diffMin/60))
+	echo "Currently test : $diffMin minutes; $diffHrs hrs" >> /tmp/date.log
 	echo "=======" >> /tmp/date.log
 }
 
@@ -19,7 +32,7 @@ if [ ! -e "/tmp/clog.log" ]
 then
     echo 1 > /tmp/clog.log
 	recordDate
-	idleAndReboot
+	idleAndReboot 300
 else
     c=`cat /tmp/clog.log`
     ((c++))
@@ -30,4 +43,5 @@ fi
 }
 
 # execution area
+beginDate
 reboot_main
